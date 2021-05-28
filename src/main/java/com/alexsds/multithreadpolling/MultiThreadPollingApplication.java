082cf36e58770966e72e800fdf92bdd204793e77
Alex Kovalenko <iamaleksandr@gmail.com>
2021-05-28 11:17:06 +0300

    init

---
+++ src/main/java/com/alexsds/multithreadpolling/MultiThreadPollingApplication.java
@@ -0,0 +1,13 @@
+package com.alexsds.multithreadpolling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreadPollingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiThreadPollingApplication.class, args);
+	}
+
+}
